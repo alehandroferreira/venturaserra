770cb3feeba8a4b5069676ed86aa4a881ffdc6f0
Alehandro Rocha <arferrir@MacBook-Air-de-Alehandro.local>
2026-02-11 16:57:00 -0300

    chore: configuracao de ferramentas

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/main/server.ts',
+    '!src/infrastructure/express/app.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  moduleNameMapper: {
+    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@application/(.*)$': '<rootDir>/src/application/$1',
+    '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
+    '^@main/(.*)$': '<rootDir>/src/main/$1',
+  },
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+};
